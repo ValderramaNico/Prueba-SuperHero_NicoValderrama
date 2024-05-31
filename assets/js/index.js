0306264d536ef59f94736e9a742a3b6d04a5ee41
spl-NicoValderrama <valderrama.nicolas@outlook.com>
2024-05-31 18:11:44 -0400

    arreglo pequeño

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,7 +21,7 @@ $(function () {
                           <hr>        
                           <p class="card-text">${heroWeightValue(hero.appearance.weight)}</p>
                           <hr>        
-                          <p class="card-text">${heroNoneValue(hero.biography.aliases)}</p>     //*! no reemplaza el valor por el string de la función 
+                          <p class="card-text">${heroNoneValue(hero.biography.aliases)}</p>
                       </div>
                   </div>
               </div>
